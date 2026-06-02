ac82d9aedd3bcd4566d1f963983d4270c2f8dbaf
Student RM4-3073 09 <dominik.panasz@outlook.com>
2026-06-02 12:49:50 +0200

    Zad3

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -6,9 +6,15 @@ namespace WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            string angularCorsPolicy = "Angular";
             // Add services to the container.
-
+            builder.Services.AddCors(CorsPolicy => 
+            {
+                CorsPolicy.AddPolicy(name: angularCorsPolicy, policy => 
+                {
+                    policy.WithOrigins("http://localhost:4109").AllowAnyMethod().AllowAnyHeader();
+                });
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -22,7 +28,7 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseCors(angularCorsPolicy);
             app.UseAuthorization();
 
 
